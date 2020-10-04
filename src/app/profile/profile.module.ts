29e6301be83a56c756e88a88c3c11eb2c6ed8a3a
rajansharma3097 <sharma.rajan3097@gmail.com>
2020-10-04 10:43:15 +0530

    first commit

---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
+import { ProfileComponent } from './profile.component';
+import { HeaderComponent } from './header/header.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    SnotifyModule,
+    NgxSpinnerModule
+  ],
+  declarations: [
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    ContactComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+  ],
+  providers: [
+    { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
+    SnotifyService,
+  ]
+})
+export class ProfileModule { }
